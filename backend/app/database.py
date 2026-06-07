724dfff3694cb6367a72087b4d23768ea9e58174
sridipseal12 <sridipsealxiib30@gmail.com>
2026-06-07 17:38:14 +0530

    docker code for load balancing

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:878906@localhost:5432/medicine_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:878906@localhost:5432/medicine_db",
+)
 
 engine = create_engine(DATABASE_URL)
 
